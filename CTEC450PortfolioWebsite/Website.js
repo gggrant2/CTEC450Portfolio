62cffd9dcb1beef527e9cc965cb7af37ff56747b
Genesis Grant <140537374+gggrant2@users.noreply.github.com>
2025-05-07 10:35:26 -0400

    Delete CTEC450PortfolioWebsite directory

--- CTEC450PortfolioWebsite/Website.js
+++
@@ -1,5 +0,0 @@
-function changeBackground() {
-  const colors = ["#f4f4f4", "#e6f7ff", "#fff0f5", "#ffffcc"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
-}
